fec4dabf28bc9fe482868f25414fe0a72c7d6afb
aruZeta <aru_hackZ.official@zohomail.eu>
2022-07-16 00:09:36 +0200

    [add] Test createTag macro

---
+++ tests/testCreateTag.nim
@@ -0,0 +1,21 @@
+import unittest
+import tagger
+
+test "Minimal tag":
+  createTag p
+
+  let tag = p:
+    id = "test"
+    "Test text"
+
+  check tag == "<p id=\"test\">Test text</p>"
+
+test "Minimal tag with multiple content fields":
+  createTag p
+
+  let tag = p:
+    id = "test"
+    "Test text, value: "
+    1
+
+  check tag == "<p id=\"test\">Test text, value: 1</p>"
