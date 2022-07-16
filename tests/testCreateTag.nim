bb446dc633a36fcce9c36235f5fac8e950ba2726
aruZeta <aru_hackZ.official@zohomail.eu>
2022-07-16 12:37:51 +0200

    [add] Test multiple params in createTag macro

--- tests/testCreateTag.nim
+++ tests/testCreateTag.nim
@@ -19,3 +19,13 @@ test "Minimal tag with multiple content fields":
     1
 
   check tag == "<p id=\"test\">Test text, value: 1</p>"
+
+test "Minimal tag with multiple params":
+  createTag p
+
+  let tag = p:
+    id = "test"
+    class = "test"
+    "Test text"
+
+  check tag == "<p id=\"test\" class=\"test\">Test text</p>"
