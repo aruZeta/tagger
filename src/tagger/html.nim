5764030a79596b35d3f5378efaca6dbc326d7af4
aruZeta <aru_hackZ.official@zohomail.eu>
2022-07-19 22:28:53 +0200

    [add] tagger/html, creates all html tags there are
    Got it from w3schools, guess those are all.

---
+++ src/tagger/html.nim
@@ -0,0 +1,126 @@
+import tagger/tagMacro
+
+# <!DOCTYPE> - needs implementation
+
+createTag a
+createTag abbr
+createTag acronym
+createTag address
+createTag applet
+createTag area, closed = false
+createTag article
+createTag aside
+createTag audio
+createTag b
+createTag base, closed = false
+createTag basefont
+createTag bdi
+createTag bdo
+createTag big
+createTag blockquote
+createTag body
+createTag br, closed = false
+createTag button
+createTag canvas
+createTag caption
+createTag center
+createTag cite
+createTag code
+createTag col, closed = false
+createTag colgroup
+createTag data
+createTag datalist
+createTag dd
+createTag hdel, tagName = "del"
+createTag details
+createTag dfn
+createTag dialog
+createTag dir
+createTag hdiv, tagName = "div"
+createTag dl
+createTag dt
+createTag em
+createTag embed, closed = false
+createTag fieldset
+createTag figcaption
+createTag figure
+createTag font
+createTag footer
+createTag form
+createTag frame
+createTag frameset
+createTag h1
+createTag h2
+createTag h3
+createTag h4
+createTag h5
+createTag h6
+createTag head
+createTag header
+createTag hr, closed = false
+createTag html
+createTag i
+createTag iframe
+createTag img, closed = false
+createTag input, closed = false
+createTag ins
+createTag kbd
+createTag label
+createTag legend
+createTag li
+createTag link, closed = false
+createTag main
+createTag map
+createTag mark
+createTag meta, closed = false
+createTag meter
+createTag nav
+createTag noframes
+createTag noscript
+createTag hobject, tagName = "object"
+createTag ol
+createTag optgroup
+createTag option
+createTag output
+createTag p
+createTag param, closed = false
+createTag picture
+createTag pre
+createTag progress
+createTag q
+createTag rp
+createTag rt
+createTag ruby
+createTag s
+createTag samp
+createTag script
+createTag section
+createTag select
+createTag small
+createTag source, closed = false
+createTag span
+createTag strike
+createTag strong
+createTag style
+createTag sub
+createTag summary
+createTag sup
+createTag svg
+createTag table
+createTag tbody
+createTag td
+createTag htemplate, tagName = "template"
+createTag textarea
+createTag tfoot
+createTag th
+createTag thead
+createTag time
+createTag title
+createTag tr
+createTag track, closed = false
+createTag tt
+createTag u
+createTag ul
+createTag hvar, tagName = "var"
+createTag video
+createTag wbr
